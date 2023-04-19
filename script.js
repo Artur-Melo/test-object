7ed37d7e939ac49a7929473327ff7fbf2c9f6067
Artur-Melo <meloartur75@gmail.com>
2023-04-19 11:58:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const object = {
+    id: 1,
+    name: "Artur",
+    age: 16,
+    city: "Maceió",
+    function: () =>{
+        return console.log(`Usuário: ${object.name}`)
+    }
+};
+
+object.function();
